f4632c905ade30627cc68e4774d888e29b5f7c50
ngetichTg <ngetichgideon6@gmail.com>
2023-10-30 10:14:59 +0300

    adding js and refactoring the view model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,7 @@ class CommentsController < ApplicationController
 
   # GET /comments/1 or /comments/1.json
   def show
+
   end
 
   # GET /comments/new
@@ -27,6 +28,7 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_to comment_url(@comment), notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
+        format.js
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
+  belongs_to :user
   belongs_to :post
+  validates_presence_of :post_id
+  validates_presence_of :body
 end
